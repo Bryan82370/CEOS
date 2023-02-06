27030539b72b879b1dbcca55e71f1814c872f870
Heller Bryan <hellerbryan.pro@gmail.com>
2023-02-06 09:29:35 +0100

    [IMP] setting up pages

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('addCategory/', views.addCategory, name='addCategory'),
+    path('addDocument/', views.addDocument, name='addDocument'),
+    path('base/', views.base, name='base'),
+    path('category/', views.category, name='category'),
+    path('document/', views.document, name='document'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def base(request):
+    return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'security/login.html')
+
+def register(request):
+    return render(request, 'security/register.html')
+
+def addCategory(request):
+    return render(request, 'addCategory.html')
+
+def addDocument(request):
+    return render(request, 'addDocument.html')
+
+def document(request):
+    return render(request, 'document.html')
+
+def category(request):
+    return render(request, 'category.html')
