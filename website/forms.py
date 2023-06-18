7867441c125309ba2128d549c2db28c0ec511770
dremaux <ldremaux@btslivh.eu>
2023-06-18 17:59:30 +0200

    inscription input -> type=password

--- website/forms.py
+++ website/forms.py
@@ -6,8 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class InscriptionForm(UserCreationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'style': 'border-radius: 20px; width: 100%;'}), label="Nom d'utilisateur")
-    password1 = forms.CharField(widget=forms.TextInput(attrs={'style': 'border-radius: 20px; width: 100%;'}), label="Nom d'utilisateur")
-    password2 = forms.CharField(widget=forms.TextInput(attrs={'style': 'border-radius: 20px; width: 100%;'}), label="Nom d'utilisateur")
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'style': 'border-radius: 20px; width: 100%;'}), label="Nom d'utilisateur")
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'style': 'border-radius: 20px; width: 100%;'}), label="Nom d'utilisateur")
 
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'password1', 'password2']
